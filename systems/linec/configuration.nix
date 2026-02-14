b00606e1c1093ccf4a82b463db7cf07db1cdef65
minec-aw <minec.aw@proton.me>
2026-02-14 01:35:28 -0500

    Laptop initiation

--- systems/linec/configuration.nix
+++ systems/linec/configuration.nix
@@ -3,8 +3,21 @@
   imports = [
     ./hardware-configuration.nix
     ../shared-configuration.nix
+    ../../homes/minec
     ../../modules
   ];
   waydroid.enable = true;
-  
+  coding.enable = true;
+  services = {
+    displayManager = {
+      sddm.enable = true;
+    };
+    plasma6.enable = true;
+    printing.enable = true;
+  };
+
+  networking = {
+    hostName = "linec";
+    networkmanager.enable = true;
+  };
 }
