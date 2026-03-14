92e36da3de9b1154cf91bec91f1fbb2cea08977c
minec-aw <minec.aw@proton.me>
2026-03-14 16:11:27 -0400

    new models

--- modules/llama-cpp/default.nix
+++ modules/llama-cpp/default.nix
@@ -30,16 +30,32 @@
                     cudaSupport = true;
                 };
                 modelsPreset = {
-                  "crow-9b" = {
-                    model = "/media/Storage/AI Models/Crow/Qwen3.5-9B-heretic-v2.Q5_K_M.gguf";
-                    mmproj = "/media/Storage/AI Models/Crow/Qwen3.5-9B-heretic-v2.BF16-mmproj.gguf";
-                    alias = "crow/crow-9b";
-                    ctx-size = "65536";
+                  "qwen3.5g-9b" = {
+                    model = "/media/Storage/AI Models/Jackrong-Qwen3.5/Gemini-3.1/Qwen3.5-9B.Q6_K.gguf";
+                    mmproj = "/media/Storage/AI Models/Jackrong-Qwen3.5/Gemini-3.1/Qwen3.5-9B.BF16-mmproj.gguf";
+                    ctx-size = "98304";
                     n-gpu-layers = "99";
                     flash-attn = "on";
                     temp = "0.6";
                     top-p = "0.95";
                     top-k = "20";
+                    cache-type-k = "bf16";
+                    cache-type-v = "bf16";
+                    min-p = "0.01";
+
+                  };
+                  "qwen3.5o-9b" = {
+                    model = "/media/Storage/AI Models/Jackrong-Qwen3.5/Opus/Qwen3.5-9B.Q6_K.gguf";
+                    mmproj = "/media/Storage/AI Models/Jackrong-Qwen3.5/Opus/Qwen3.5-9B.BF16-mmproj.gguf";
+                    ctx-size = "98304";
+                    n-gpu-layers = "99";
+                    flash-attn = "on";
+                    temp = "0.6";
+                    top-p = "0.95";
+                    top-k = "20";
+                    cache-type-k = "bf16"; 
+                    cache-type-v = "bf16";
+                    min-p = "0.01";
                   };
                 };
             };
