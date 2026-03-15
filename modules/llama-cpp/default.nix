6cf686adf0ec2b4869a39be74f1946187440f109
minec-aw <minec.aw@proton.me>
2026-03-14 20:17:32 -0400

    cleanup unused options

--- modules/llama-cpp/default.nix
+++ modules/llama-cpp/default.nix
@@ -23,43 +23,6 @@
                 port = 10086;
             };
             
-            /*llama-cpp = {
-                enable = true;
-                host = "0.0.0.0";
-                port = 10087;
-                package = pkgs.callPackage ./llamacpp.nix {
-                    cudaSupport = true;
-                };
-                modelsPreset = {
-                  "qwen3.5g-9b" = {
-                    model = "/media/Storage/AI Models/Jackrong-Qwen3.5/Gemini-3.1/Qwen3.5-9B.Q6_K.gguf";
-                    mmproj = "/media/Storage/AI Models/Jackrong-Qwen3.5/Gemini-3.1/Qwen3.5-9B.BF16-mmproj.gguf";
-                    ctx-size = "98304";
-                    n-gpu-layers = "99";
-                    flash-attn = "on";
-                    temp = "0.6";
-                    top-p = "0.95";
-                    top-k = "20";
-                    cache-type-k = "bf16";
-                    cache-type-v = "bf16";
-                    min-p = "0.01";
-
-                  };
-                  "qwen3.5o-9b" = {
-                    model = "/media/Storage/AI Models/Jackrong-Qwen3.5/Opus/Qwen3.5-9B.Q6_K.gguf";
-                    mmproj = "/media/Storage/AI Models/Jackrong-Qwen3.5/Opus/Qwen3.5-9B.BF16-mmproj.gguf";
-                    ctx-size = "98304";
-                    n-gpu-layers = "99";
-                    flash-attn = "on";
-                    temp = "0.6";
-                    top-p = "0.95";
-                    top-k = "20";
-                    cache-type-k = "bf16"; 
-                    cache-type-v = "bf16";
-                    min-p = "0.01";
-                  };
-                };
-            };*/
         };
     };
 }
